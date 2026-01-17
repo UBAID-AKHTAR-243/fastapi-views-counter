d54570fc4a909a853febb9c950b594cb30ce5ae0
UBAID-AKHTAR-243 <60124351+UBAID-AKHTAR-243@users.noreply.github.com>
2026-01-17 11:38:47 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const BASE_URL = "https://fastapi-views-counter.onrender.com"; // replace with your Render URL
+
+async function loadViews(reelId) {
+  const response = await fetch(`${BASE_URL}/views/${reelId}`);
+  const data = await response.json();
+  document.getElementById("view-count").innerText = data.views;
+}
+
+async function addView(reelId) {
+  const response = await fetch(`${BASE_URL}/views/${reelId}`, { method: "POST" });
+  const data = await response.json();
+  document.getElementById("view-count").innerText = data.views;
+}
+
+// When page loads, increment and show views
+addView("reel1");
